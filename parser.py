bf60f0156cefe957ee1b85d1f9d04228b7ed13d8
Chan <chanakashirley@ymail.com>
2014-05-19 17:55:39 -0700

    Deleted unnessary stuff

--- parser.py
+++ parser.py
@@ -3,34 +3,7 @@ import urlparse
 import urllib
 import nutrition
 from bs4 import BeautifulSoup
-"""
-def nutrition_parse(url):
-	calorie = ""
-	try:
-		htmltext = urllib.urlopen(url).read()
-	except:
-		print("Error!")
-		print(url)
-	soup = BeautifulSoup(htmltext)
 
-	food = soup.find("form").get_text(strip=True)
-	food.encode()
-	
-	gen = (line for line in soup.stripped_strings if "Calories" in line)
-	
-	for line in gen:
-		if not "Fat" in line:
-			calo = line.strip()
-			calo = ''.join([i for i in calo if i.isdigit()])
-			#calo = int(calo)
-		else:
-			fat = line.strip()
-			fat = ''.join([i for i in calo if i.isdigit()])
-	calorie = food + ' ' + "Calories: " + calo + ", Calories from Fat: " + fat
-
-			#print(calo)
-	return (calorie, int(calo), int(fat))
-"""
 
 def create_food(url):
 	try:
